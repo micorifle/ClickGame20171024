edaccae73bcbc1009a2252a43d76bf605535eed2
Makoto Miura <nkc.makoto.miura@gmail.com>
2017-10-26 10:08:29 +0900

    スクリプトとプレハブの追加

---
+++ Assets/Controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Ball")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Generator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            bullet.GetComponent<Controller>().Shoot(worldDir.normalized * 20000);
+        }
+	}
+}
